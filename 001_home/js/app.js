d7ea11f06f5cc70820be4652a30e1505a5ab7a41
Chris Canning <chris.canning19291@hotmail.co.uk>
2020-10-15 22:27:22 +0100

    JavaScript functionality completed

--- 001_home/js/app.js
+++ 001_home/js/app.js
@@ -23,4 +23,40 @@ function init() {
 
 init();
 
-// console.log(moActive);
+document.querySelector('main').addEventListener('click', onClick);
+
+function onClick() {
+    if(commActive.classList.contains('buttonBackgroundActive'), commandActive.classList.contains('activeButton')) {
+        commActive.classList.remove('buttonBackgroundActive');
+        commandActive.classList.remove('activeButton');
+        moveActive.classList.add('buttonBackgroundActive');
+        movementActive.classList.add('activeButton');
+    } else if(moveActive.classList.contains('buttonBackgroundActive'), movementActive.classList.contains('activeButton')) {
+        moveActive.classList.remove('buttonBackgroundActive');
+        movementActive.classList.remove('activeButton');
+        psyActive.classList.add('buttonBackgroundActive');
+        psychicActive.classList.add('activeButton');
+    } else if(psyActive.classList.contains('buttonBackgroundActive'), psychicActive.classList.contains('activeButton')) {
+        psyActive.classList.remove('buttonBackgroundActive');
+        psychicActive.classList.remove('activeButton');
+        shootActive.classList.add('buttonBackgroundActive');
+        shootingActive.classList.add('activeButton');
+    } else if(shootActive.classList.contains('buttonBackgroundActive'), shootingActive.classList.contains('activeButton')) {
+        shootActive.classList.remove('buttonBackgroundActive');
+        shootingActive.classList.remove('activeButton');
+        chaActive.classList.add('buttonBackgroundActive');
+        chargeActive.classList.add('activeButton');
+    } else if(chaActive.classList.contains('buttonBackgroundActive'), chargeActive.classList.contains('activeButton')) {
+        chaActive.classList.remove('buttonBackgroundActive');
+        chargeActive.classList.remove('activeButton');
+        fiActive.classList.add('buttonBackgroundActive');
+        fightActive.classList.add('activeButton');
+    } else if(fiActive.classList.contains('buttonBackgroundActive'), fightActive.classList.contains('activeButton')) {
+        fiActive.classList.remove('buttonBackgroundActive');
+        fightActive.classList.remove('activeButton');
+        moActive.classList.add('buttonBackgroundActive');
+        moraleActive.classList.add('activeButton');
+    } else {
+        init();
+    }
+}

4efbd098b3c6aaa9064d9844a6c0ffb9582b549f
Chris Canning <chris.canning19291@hotmail.co.uk>
2020-10-15 03:09:52 +0100

    Minor id changes and full init code declared for beginning state

--- 001_home/js/app.js
+++ 001_home/js/app.js
@@ -1,11 +1,26 @@
 const commActive = document.getElementById('comm');
 const commandActive = document.getElementById('command');
+const moveActive = document.getElementById('move');
+const movementActive = document.getElementById('movement');
+const psyActive = document.getElementById('psy');
+const psychicActive = document.getElementById('psychic');
+const shootActive = document.getElementById('shoot');
+const shootingActive = document.getElementById('shooting');
+const chaActive = document.getElementById('cha');
+const chargeActive = document.getElementById('charge');
+const fiActive = document.getElementById('fi');
+const fightActive = document.getElementById('fight');
+const moActive = document.getElementById('mo');
+const moraleActive = document.getElementById('morale');
 
 function init() {
-    commandActive.classList.remove('activeButton');
+    commActive, moveActive, psyActive, shootActive, chaActive, fiActive, moActive.classList.remove('buttonBackgroundActive');
+    commandActive, movementActive, psychicActive, shootingActive, chargeActive, fightActive, moraleActive.classList.remove('activeButton');
     commandActive.classList.add('activeButton');
-    commActive.classList.remove('buttonBackground');
     commActive.classList.add('buttonBackgroundActive');
+    moveActive, psyActive, shootActive, chaActive, fiActive, moActive.classList.add('buttonBackground');
 }
 
 init();
+
+// console.log(moActive);
